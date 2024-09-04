feff6f88af594e8a41f3694fcfca7c64ea18ab40
naiknetra <naiknetra5@gmail.com>
2024-09-04 19:25:48 -0400

    updated javascrpt

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+// Access toggle switch HTML element
+//const themeSwitcher = document.querySelector('#theme-switcher');
+const btnMexican = document.querySelector('#mexican');
+// Listen for a click event on toggle switch
+btnMexican.addEventListener('click', function () {
+  // If mode is dark, apply light background
+ 
+});
+
+const btnIndian = document.querySelector('#indian');
+// Listen for a click event on toggle switch
+btnIndian.addEventListener('click', function () {
+  // If mode is dark, apply light background
+ 
+});
+
+const btnItalian = document.querySelector('#italian');
+// Listen for a click event on toggle switch
+btnItalian.addEventListener('click', function () {
+  // If mode is dark, apply light background
+ 
+});
+
+const btnChinese = document.querySelector('#chinese');
+// Listen for a click event on toggle switch
+btnChinese.addEventListener('click', function () {
+  // If mode is dark, apply light background
+ 
+});

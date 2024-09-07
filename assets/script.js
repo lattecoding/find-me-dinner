c907281c0ed060050520b2dcf9742d3485256ea4
naiknetra <naiknetra5@gmail.com>
2024-09-07 01:16:53 -0400

    updated javascript

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,58 @@
-//const restaurants = {
-  //italian: ['Restaurant A', 'Restaurant B', 'Restaurant C'],
-  //mexican: ['Restaurant X', 'Restaurant Y', 'Restaurant Z'],
-  //indian: ['Restaurant P', 'Restaurant Q', 'Restaurant R']
-//};
+
+
+const restaurants = [
+  {
+  type: "Italian",
+  name: "Bertucci's"
+},
+  {
+    type: "Italian",
+    name: "Olive Garden"
+  },
+  {
+    type: "Italian",
+    name: "Angelos"
+  },
+  {
+    type: "Mexican",
+    name: "Boxcar Cantinna"
+  },
+  {
+    type: "Mexican",
+    name: "Brasitas"
+  },
+  {
+    type: "Mexican",
+    name: "Mexicue"
+  },  
+  {
+    type: "Indian",
+    name: "Chutney"
+  },
+  {
+    type: "Indian",
+    name: "Tawa"
+  },
+  {
+    type: "Indian",
+    name: "Musafir"
+  },
+  {
+    type: "Chinese",
+    name: "First wok"
+  },
+  {
+    type: "Chinese",
+    name: "Smoky Noodles"
+  },
+  {
+    type: "Chinese",
+    name: "China Palace"
+  }
+];
+
+const selectedRestType = "";
+
 
 //function findRestaurants() {
  // const restaurantType = document.getElementById('restaurantType').value;
@@ -22,73 +72,54 @@
 
 
 
-const restaurantsByCuisine = {
-  "Italian": [
-      "Dominos",
-      "Olive Garden",
-      "Angelos"
-  ],
-  "Mexican": [
-      "Frontera",
-      "Taco Bell",
-      "Mexican Grill"
-  ]
-  "Indian": [
-    "Restauant Indian 1",
-    "Restuarant indiano 2",
-    "Restuarnt india 3"
-  ]
-  "Chinese": [
-    "Chinese Food 1",
-    "China Palace 2",
-    "China restuarnt 3"
-  ]
-};
-
 //const zipCode = document.getElementById('zipCode').value;
 
 function appendRestaurantsByCuisine(cuisineType) {
-  let restaurantContainer = document.getElementById("restaurant-container");
-  let restaurants = restaurantsByCuisine[cuisineType];
+ selectedRestType = cuisineType;
+ console.log("selectedRestType:"+selectedRestType);
+  // let restaurantContainer = document.getElementById("restaurant-container");
+  // let restaurants = restaurantsByCuisine[cuisineType];
 
-  restaurants.forEach(restaurantName => {
-      let restaurantDiv = document.createElement("div");
-      restaurantDiv.classList.add("restaurant");
+  // restaurants.forEach(restaurantName => {
+  //     let restaurantDiv = document.createElement("div");
+  //     restaurantDiv.classList.add("restaurant");
 
-      let nameElement = document.createElement("h2");
-      nameElement.textContent = restaurantName;
+  //     let nameElement = document.createElement("h2");
+  //     nameElement.textContent = restaurantName;
 
-      restaurantDiv.appendChild(nameElement);
+  //     restaurantDiv.appendChild(nameElement);
 
-      restaurantContainer.appendChild(restaurantDiv);
-  });
+  //     restaurantContainer.appendChild(restaurantDiv);
+  // });
 }
 
-// Call the function to append Italian restaurants
-appendRestaurantsByCuisine("Italian");
+
 
 const btnMexican = document.querySelector('#mexican');
 btnMexican.addEventListener('click', function () {
 appendRestaurantsByCuisine("Mexican");
 });
 
-// const btnIndian = document.querySelector('#indian');
+const btnIndian = document.querySelector('#indian');
 // // Listen for a click event on toggle switch
-// btnIndian.addEventListener('click', function () {
-//   // If mode is dark, apply light background
- 
-// });
+btnIndian.addEventListener('click', function () {
+  appendRestaurantsByCuisine("Indian");
 
-// const btnItalian = document.querySelector('#italian');
-// // Listen for a click event on toggle switch
-// btnItalian.addEventListener('click', function () {
-//   // If mode is dark, apply light background
  
-// });
+});
 
-// const btnChinese = document.querySelector('#chinese');
-// // Listen for a click event on toggle switch
-// btnChinese.addEventListener('click', function () {
-//   // If mode is dark, apply light background
+const btnItalian = document.querySelector('#italian');
+
+ btnItalian.addEventListener('click', function () {
+  appendRestaurantsByCuisine("Italian");
+
  
-// });
+});
+
+ const btnChinese = document.querySelector('#chinese');
+
+btnChinese.addEventListener('click', function () {
+  appendRestaurantsByCuisine("Chinese");
+
+ 
+ });
